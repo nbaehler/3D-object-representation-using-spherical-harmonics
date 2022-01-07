39552424bae025a79a6fd86b9ceaa12199e6bb03
nbaehler <nicolas.bahler@epfl.ch>
2022-01-07 09:09:55 +0100

    Adapt download script

--- scripts/download.sh
+++ scripts/download.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 
-if [[ ! -e $1 ]]; then
-    echo "$1 is not an existing file or directory"
-    exit 1
-fi
-
 scp -r nbaehler@fidis.epfl.ch:/home/nbaehler/workspace/3D-object-representation-using-spherical-harmonics/"$1" ~/Downloads
